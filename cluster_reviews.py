d2618a81b2198d43e33ddde69de13340b0447504
Wok <woctezuma@users.noreply.github.com>
2017-12-28 00:41:21 +0100

    Export a simple function to get a data frame containing features of English reviews

---
+++ cluster_reviews.py
@@ -0,0 +1,12 @@
+from describe_reviews import analyzeAppIDinEnglish
+
+def main():
+    appID_list = [723090, 639780, 573170]
+
+    appID = str( appID_list[0] )
+    analyzeAppIDinEnglish(appID)
+
+    return
+
+if __name__ == "__main__":
+    main()
