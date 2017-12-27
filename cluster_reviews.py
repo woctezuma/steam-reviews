e837fbd1f4af34dba7dad51a5e84efb9fff407b3
Wok <woctezuma@users.noreply.github.com>
2017-12-28 00:55:02 +0100

    Function to retrieve the content of a review. Use strings for appID. Use Fidel Dungeon Rescue as example.

--- cluster_reviews.py
+++ cluster_reviews.py
@@ -1,10 +1,15 @@
-from describe_reviews import analyzeAppIDinEnglish
+from describe_reviews import analyzeAppIDinEnglish, getReviewContent
 
 def main():
-    appID_list = [723090, 639780, 573170]
+    appID_list = ["723090", "639780", "573170"]
 
-    appID = str( appID_list[0] )
-    analyzeAppIDinEnglish(appID)
+    appID = appID_list[-1]
+
+    df = analyzeAppIDinEnglish(appID)
+
+    review_content = getReviewContent(appID, "38674426")
+
+    print(review_content)
 
     return
 
