0819dbc20e6e63b50da24ce9038f2c3a0d9f366e
Wok <woctezuma@users.noreply.github.com>
2017-12-27 11:29:29 +0100

    Working code to download reviews. NB: There is sometimes a discrepancy between expected number of reviews and actual number of downloaded reviews.

--- reviews.py
+++ reviews.py
@@ -1,6 +1,6 @@
 #!/usr/env/bin python
 
-"""Create games.json using ID's in idlist.csv."""
+"""Create games.json and review_APPID.json using APPID's in idlist.csv."""
 
 # Heavily inspired from: https://raw.githubusercontent.com/CraigKelly/steam-data/master/data/games.py
 
@@ -15,25 +15,24 @@ def parse_id(i):
     """Since we deal with both strings and ints, force appid to be correct."""
     try:
         return int(str(i).strip())
-    except:
+    except ValueError:
         return None
 
 
 def id_reader():
     """Read the previous created idlist.csv."""
-    with open("idlist.csv") as basefile:
-        reader = csv.DictReader(basefile)
+    with open("idlist.txt") as basefile:
+        reader = csv.reader(basefile)
         for row in reader:
-            yield parse_id(row['ID']), row['Name']
+            yield parse_id(row[0])
 
 
 def previous_results():
     """Return a set of all previous found ID's."""
     all_ids = set()
-    with open("games.json") as f:
+    with open("idprocessed.txt", "r") as f:
         for line in f:
-            rec = json.loads(line)
-            appid = parse_id(rec.get('query_appid', None))
+            appid = parse_id(line)
             if appid:
                 all_ids.add(appid)
     return all_ids
@@ -43,57 +42,100 @@ def main():
     """Entry point."""
     logging.basicConfig(level=logging.DEBUG)
     logging.getLogger('requests').setLevel(logging.DEBUG)
-    log = logging.getLogger('games')
+    log = logging.getLogger(__name__)
 
     defaults = {
-        'cc': 'US',
-        'l': 'english',
-        'v': '1',
+        # Copied from: https://gist.github.com/adambuczek/95906b0c899c5311daeac515f740bf33
+        'json': '1',
+        'language': "all",  # e.g. english or schinese
+        'filter': "recent",  # e.g. funny or helpful
+        'review_type': "all",
+        'purchase_type': "all",
     }
     log.info("Default parameters: %s", repr(defaults))
 
-    URL = "http://store.steampowered.com/api/appdetails/"
-    LIMIT = 150
-    WAIT_TIME = (5 * 60) + 10  # 5 minutes plus a cushion
+    api_url = "http://store.steampowered.com/appreviews/"
+    query_limit = 150
+    wait_time = (5 * 60) + 10  # 5 minutes plus a cushion
 
-    log.info("Getting previous results from games.json")
+    max_num_reviews = pow(10, 4)
+
+    log.info("Getting previous results from reviews.json")
     previos_ids = previous_results()
 
-    log.info("Opening games.json")
-    with open("games.json", "a") as f:
-        count, batch_count = 0, 0
+    log.info("Opening reviews.json")
+    with open("idprocessed.txt", "a") as f:
+        query_count = 0
+        game_count = 0
+        game = dict()
 
         log.info("Opening idlist.csv")
-        for appid, appname in id_reader():
+        for appid in id_reader():
             if appid in previos_ids:
                 log.info("Skipping previously found id %d", appid)
                 continue
 
+            url = api_url + str(appid)
+
             req_data = dict(defaults)
             req_data['appids'] = str(appid)
 
-            resp_data = requests.get(URL, params=req_data)
+            review_dict = dict()
+            reviews = []
 
-            # Loop is an artifact of Steam's past suppot for multiple appid
-            # queries: we use it to insure we got back what we asked for - the
-            # details for appid
-            for result_appid, game in resp_data.json().items():
-                if parse_id(result_appid) != appid:
-                    log.error("Invalid - %s does not match requested id %d", result_appid, appid)
+            # Initialize
+            offset = 0
+            num_reviews = max_num_reviews
+            try_count = 0
+            while (offset < num_reviews) and (try_count < 3):
+                req_data['start_offset'] = str(offset)
 
-                game['query_appid'] = appid
-                game['query_appname'] = appname
-                f.write(json.dumps(game) + '\n')
+                resp_data = requests.get(url, params=req_data)
 
-                count += 1
+                result = resp_data.json()
 
-            batch_count += 1
-            if batch_count >= LIMIT:
-                log.info("batch count is %d, waiting for %d secs", batch_count, WAIT_TIME)
-                time.sleep(WAIT_TIME)
-                batch_count = 0
+                reviews.extend(result["reviews"])
 
-    log.info("Game records written: %d", count)
+                num_reviews_with_this_request = result["query_summary"]["num_reviews"]
+                offset += num_reviews_with_this_request
+
+                if num_reviews_with_this_request == 0:
+                    try_count += 1
+
+                if num_reviews == max_num_reviews:
+                    # Real number of reviews for the given appID
+                    num_reviews = result["query_summary"]["total_reviews"]
+
+                    print(result["query_summary"])
+
+                    # To be saved to JSON
+                    review_dict["query_summary"] = result["query_summary"]
+
+                query_count += 1
+
+                if query_count >= query_limit:
+                    log.info("query count is %d, waiting for %d secs", query_count, wait_time)
+                    time.sleep(wait_time)
+                    query_count = 0
+
+            review_dict["reviews"] = dict()
+            for review in reviews:
+                reviewID = review["recommendationid"]
+                review_dict["reviews"][reviewID] = review
+
+            output_file = "review_" + str(appid) + ".json"
+
+            with open(output_file, "w") as g:
+                g.write(json.dumps(review_dict) + '\n')
+
+            log.info("Review records written for %s: %d (expected: %d)",
+                     appid, len(review_dict["reviews"]), num_reviews)
+
+            game_count += 1
+
+            f.write(str(appid) + '\n')
+
+    log.info("Game records written: %d", game_count)
 
 
 if __name__ == "__main__":
