1053bed105ebf2076ed88845d9dd6eb83975ae60
Wok <woctezuma@users.noreply.github.com>
2017-12-28 01:00:22 +0100

    Hard-code test parameters

--- cluster_reviews.py
+++ cluster_reviews.py
@@ -1,19 +1,22 @@
 from describe_reviews import analyzeAppIDinEnglish, getReviewContent
 
 def test_imported_module():
-    appID_list = ["723090", "639780", "573170"]
+    appID = "573170"
+    reviewID = "38674426"
 
-    appID = appID_list[-1]
-
-    df = analyzeAppIDinEnglish(appID)
-
-    review_content = getReviewContent(appID, "38674426")
+    review_content = getReviewContent(appID, reviewID)
 
     print(review_content)
 
     return
 
 def main():
+    appID_list = ["723090", "639780", "573170"]
+
+    appID = appID_list[-1]
+
+    df = analyzeAppIDinEnglish(appID)
+
     return
 
 if __name__ == "__main__":
