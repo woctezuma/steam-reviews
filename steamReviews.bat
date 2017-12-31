b077952c400d03104fd0f8ff1a9daf272f797748
Wok <woctezuma@users.noreply.github.com>
2017-12-31 23:55:32 +0100

    .bat file

--- steamReviews.bat
+++ steamReviews.bat
@@ -1,4 +1,5 @@
 @echo off
 pythonw.exe download_reviews.py %*
-python.exe describe_reviews.py %1 723090
-python.exe cluster_reviews.py %1 723090
+REM python.exe describe_reviews.py %1 723090
+REM python.exe cluster_reviews.py %1 723090
+python.exe identify_joke_reviews.py %1 723090
