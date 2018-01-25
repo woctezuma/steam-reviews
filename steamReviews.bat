7a12bede28deace0ca864a2dd3ba39951a48d298
Wok <woctezuma@users.noreply.github.com>
2018-01-25 19:55:39 +0100

    Display more info while downloading reviews with .bat file ; Comment out hype estimation in .bat file

--- steamReviews.bat
+++ steamReviews.bat
@@ -1,7 +1,7 @@
 @echo off
-pythonw.exe download_reviews.py %*
+python.exe download_reviews.py %*
 REM python.exe describe_reviews.py %1 723090
 REM python.exe cluster_reviews.py %1 723090
 REM python.exe identify_joke_reviews.py %1 723090
-python.exe estimate_hype.py %*
+REM python.exe estimate_hype.py %*
 python.exe analyze_language.py %*
