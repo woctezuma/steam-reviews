d24619f2633e0c22a79eff671bc385abbf764e0d
Wok <woctezuma@users.noreply.github.com>
2018-05-21 19:48:55 +0200

    Use num_owners instead of num_players, due to SteamSpy's limitations

--- check_correlation.py
+++ check_correlation.py
@@ -10,17 +10,17 @@ import matplotlib.pyplot as plt
 def get_x_y():
     steam_spy_dict = steamspypi.load()
 
-    num_players_list = []
+    num_owners_list = []
     num_reviews_list = []
 
     for appID in steam_spy_dict.keys():
-        num_players = steam_spy_dict[appID]['players_forever']
+        num_owners = steam_spy_dict[appID]['owners']
         num_reviews = sum(steam_spy_dict[appID][keyword] for keyword in ['positive', 'negative'])
 
-        num_players_list.append(num_players)
+        num_owners_list.append(num_owners)
         num_reviews_list.append(num_reviews)
 
-    return num_players_list, num_reviews_list
+    return num_owners_list, num_reviews_list
 
 
 def get_log_list(vec):
@@ -30,15 +30,15 @@ def get_log_list(vec):
 
 
 def main():
-    (num_players_list, num_reviews_list) = get_x_y()
+    (num_owners_list, num_reviews_list) = get_x_y()
 
-    log_num_players_list = get_log_list(num_players_list)
+    log_num_owners_list = get_log_list(num_owners_list)
     log_num_reviews_list = get_log_list(num_reviews_list)
 
     fig, ax = plt.subplots()
-    ax.scatter(log_num_players_list, log_num_reviews_list)
+    ax.scatter(log_num_owners_list, log_num_reviews_list)
 
-    ax.set_xlabel('log(1+#players)')
+    ax.set_xlabel('log(1+#owners)')
     ax.set_ylabel('log(1+#reviews)')
 
     xmax = 8
